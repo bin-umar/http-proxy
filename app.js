9a74ecf0eb13ab453e4b63975319f9cf9c723bd0
bin-umar <bin-umar@mail.ru>
2019-10-03 13:42:41 +0300

    Fixed HTTPS bug && added Readme

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const shelljs = require('shelljs');
 const sqlite3 = require('sqlite3');
 const fs = require('fs');
 const path = require('path');
-
+const net = require('net');
+const url = require('url');
+const requestModule = require('request');
 
 /////////// DB
 const db = new sqlite3.Database('database.sqlite3', (err) => {
@@ -16,16 +18,6 @@ const db = new sqlite3.Database('database.sqlite3', (err) => {
     }
 });
 
-const args = process.argv.slice(2);
-
-if (args.includes('-rr')) {
-    db.all('SELECT * FROM requests', (err, results) => {
-        console.log(results);
-    });
-
-    return;
-}
-
 const sql = `
     CREATE TABLE IF NOT EXISTS requests (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -33,11 +25,43 @@ const sql = `
 
 db.run(sql);
 
+const saveToDb = (request) => {
+    db.run(
+        'INSERT INTO requests (request) VALUES (?)',
+        [JSON.stringify(request)]
+    );
+};
+
+///////// ReRequest
+
+const reRequest = (dbResult) => {
+    const options = JSON.parse(dbResult.request);
+    console.log('repeat http request on ', options.path);
+    requestModule(options.path, options, (err, response, body) => console.log('result of rerequest', body));
+};
+
+const args = process.argv[2] || '';
+if (args.includes('-rr')) {
+    const id = parseInt(args.split('=')[1]);
+
+    if (!isNaN(id)) {
+        db.get('SELECT * FROM requests WHERE id=?', [id], (err, result) => {
+            if (err || !result) return;
+            reRequest(result);
+        });
+    } else {
+        db.all('SELECT * FROM requests', (err, results) => {
+            if (err || !results.length) return;
+            results.forEach(result => reRequest(result));
+        });
+    }
+
+    return;
+}
 
 ///////// HTTPS Server
 
-const certsDirectory = 'certifications';
-const root = 'http-proxy';
+const certsDirectory = 'certificates';
 
 const getCommand = ({
         country,
@@ -47,9 +71,9 @@ const getCommand = ({
     }) => `
       cd ${certsDirectory};
       openssl genrsa -out ${domain}.key 2048;
-      openssl req -new -sha256 -key ${domain}.key -subj "/C=${country}/ST=${city}/O=${organization}, Inc./CN=${domain}" -out ${domain}.csr;
+      openssl req -new -sha256 -key ${domain}.key -subj '/C=${country}/ST=${city}/O=${organization}, Inc./CN=${domain}' -out ${domain}.csr;
       openssl req -in ${domain}.csr -noout -text;
-      openssl x509 -req -in ${domain}.csr -CA ${root}/rootCA.crt -CAkey ${root}/rootCA.key -CAcreateserial -out ${domain}.crt -days 500 -sha256;
+      openssl x509 -req -in ${domain}.csr -CA ../rootCA.crt -CAkey ../rootCA.key -CAcreateserial -out ${domain}.crt -days 500 -sha256;
       openssl x509 -in ${domain}.crt -text -noout;
 `;
 
@@ -85,52 +109,46 @@ const SNICallback = (domain, cb) => {
     cb(null, context);
 };
 
-const httpsServer = https.createServer({SNICallback}, (req, res) => {
-    console.log('https', req.url);
-
-    const request = {
-        host: req.headers.host,
-        method: req.method,
-        path: req.url,
-        headers: req.headers
-    };
-
-    const proxyReq = https.request(request);
-    proxyReq.addListener('response',  (proxyRes) => {
-        proxyRes.addListener('data', (chunk) => res.write(chunk, 'binary'));
-        proxyRes.addListener('end', () => res.end());
-
-        res.writeHead(proxyRes.statusCode, proxyRes.headers);
-    });
-
-    req.addListener('data', (chunk) => proxyReq.write(chunk, 'binary'));
-    req.addListener('end', () => proxyReq.end());
-});
-
-httpsServer.on('tlsClientError',  (e) => console.log(e) );
-
-httpsServer.listen(443,  () => {
-    console.log(`HTTPS Server is listening at address 443`);
-});
+https
+    .createServer({SNICallback}, (req, res) => requestHandler(req, res, true))
+    .on('tlsClientError',  (e) => console.log(e))
+    .listen(443,  () => console.log(`HTTPS Server started listening on port 443`));
 
 ///////////// HTTP Server
-const server = http.createServer((req, res) => {
-    console.log('http', req.url);
+    
+http
+    .createServer((req, res) => requestHandler(req, res, false))
+    .on('connect', (req, cltSocket, head) => {
+        const srvUrl = url.parse(`http://${req.url}`);
+        const srvSocket = net.connect(
+            srvUrl.port,
+            srvUrl.hostname,
+            () => {
+                cltSocket.write('HTTP/1.1 200 Connection Established\r\n'
+                    + 'Proxy-agent: Node.js-Proxy\r\n'
+                    + '\r\n');
+                srvSocket.write(head);
+                srvSocket.pipe(cltSocket).on('error', (e) => console.log('srvSocket', e));
+                cltSocket.pipe(srvSocket).on('error', (e) => console.log('cltSocket', e));
+            },
+        );
+    })
+    .listen(9000, () => console.log('HTTP Server started listening on port 9000'));
 
-    const request = {
-        port: 80,
+//// Request Handler
+
+const requestHandler = (req, res, isSecure) => {
+    const options = {
+        port: isSecure ? 443 : 80,
         host: req.headers.host,
         method: req.method,
         path: req.url,
         headers: req.headers
     };
 
-    db.run(
-        'INSERT INTO requests (request) VALUES (?)',
-        [JSON.stringify(request)]
-    );
+    saveToDb(options);
 
-    const proxyReq = http.request(request);
+    const proxyReq = isSecure ? https.request(options) : http.request(options);
     proxyReq.addListener('response',  (proxyRes) => {
         proxyRes.addListener('data', (chunk) => res.write(chunk, 'binary'));
         proxyRes.addListener('end', () => res.end());
@@ -140,8 +158,4 @@ const server = http.createServer((req, res) => {
 
     req.addListener('data', (chunk) => proxyReq.write(chunk, 'binary'));
     req.addListener('end', () => proxyReq.end());
-});
-
-server.listen(8000, () => {
-    console.log("Server started listening on port 9000");
-});
+};
